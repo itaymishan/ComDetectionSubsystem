9000698dbc7eb148ab5597bc0dfdec5b9e29c26a
Itay Mishan <mishan.itay@gmail.com>
2016-02-25 20:03:23 -0500

    Update StateMachine.cpp

--- CommercialDetection/StateMachine.cpp
+++ CommercialDetection/StateMachine.cpp
@@ -26,13 +26,11 @@ void StateMachine::SetState(eSTATE eState)
 		{
 			printf("Current state is: eNEGATIVE_IMAGE_FOUND_STATE\n");
 			m_eCurrentState = eNEGATIVE_IMAGE_FOUND_STATE;
-			//char *data = "cid=1&start=1";
 			std::string buffer = "cid=" + ConfigurationManager::GetInstance().activeChannel + "&" + "start=1";
 
 			std::cout << "\n---------------------------------------------\nSend Commercial Start!!!\n---------------------------------------------\n" << std::endl;
 			std::cout << buffer << std::endl;
 			m_httpAdapter->SendHttpPostRequestWithData(ConfigurationManager::GetInstance().commercialsUrl, buffer);
-			//m_httpAdapter->SendHttpPostRequestWithData("http://46.121.98.47:7879/comfree/dev/commercials.php", buffer);
 			AppLogger::GetInstance().WriteLogEntry("Send Commercial Start");
 			
 		}
